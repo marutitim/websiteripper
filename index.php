24e4f7980144e60bca78cbdd4a52eb319905204c
marutitim <simiyuutim@gmail.com>
2016-09-14 14:04:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<html>
+    <head>
+        <title>Data Mining::Home</title>
+        <link rel="shortcut icon" href="Views/imgs/data.png"  type="image/x-icon" >
+        <meta charset="windows-1252">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" type="text/css" href="Controller/include/dhtmlx6/skins/web/dhtmlx.css"/> 
+        <link rel="stylesheet" type="text/css" href="Controller/include/dhtmlx6/skins/terrace/dhtmlx.css"/> 
+        <link rel="stylesheet" type="text/css" href="Views/css/fontawesome/css/font-awesome.min.css"/> 
+        <script src="Views/jquery/jquery-2.2.1.min.js"></script>
+        <script src="Controller/include/dhtmlx6/codebase/dhtmlx.js"></script>
+        <style>
+            #layout{
+                width:100%;
+                height:100%;  
+                padding: 0px;
+                margin:0px;
+            }
+             .fa
+            {
+                color:#3da0e3; 
+            }
+            .bcf-toolbar-icons
+            {
+                color:#3da0e3; 
+            }
+        </style>
+    </head>
+    <body  style="width:100%;height:100%;">
+        <div id="layout">
+            <script src="Views/js/main.js"></script> 
+            <script src="Views/js/web_grid.js"></script>        
+        </div>
+    </body>
+</html>
